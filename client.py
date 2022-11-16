99d4e26baa59fb30116c54fd290e5393d5fbd2b6
Sahil Chaudhary <sahil280114@gmail.com>
2022-11-16 11:35:27 +0100

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import os
 api_key = os.environ.get("API_KEY")
 model_key = os.environ.get("MODEL_KEY")
 
-model_inputs = {'prompt': 'My favorite part about working with AI is'}
+model_inputs = {'prompt': 'Scaled dot product attention:\n\n\\['}
 
 model_outputs = banana.run(api_key, model_key, model_inputs)
 
