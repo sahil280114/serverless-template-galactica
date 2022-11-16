ca6cfb6f6c37a0ce4ef19b36b775332a14bfb258
sahil280114 <sahil280114@gmail.com>
2022-11-16 10:56:13 +0000

    update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import os
 api_key = os.environ.get("API_KEY")
 model_key = os.environ.get("MODEL_KEY")
 
-model_inputs = {'prompt': 'Scaled dot product attention:\n\n\\['}
+model_inputs = {'prompt': 'Scaled dot product attention:\n\n\\[',"max_length":60,"top_p":0.7,"new_doc":False}
 
 model_outputs = banana.run(api_key, model_key, model_inputs)
 
